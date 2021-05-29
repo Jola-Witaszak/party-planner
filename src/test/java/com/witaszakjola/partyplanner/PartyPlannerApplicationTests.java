e07a315ba3918e1dde319d012f7cc00a9f198460
Jola-Witaszak <jola-w@o2.pl>
2021-05-29 16:32:48 +0200

    init commit

---
+++ src/test/java/com/witaszakjola/partyplanner/PartyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.witaszakjola.partyplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
